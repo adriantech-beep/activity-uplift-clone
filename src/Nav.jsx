f36cbc8a20f6dffca6582445110981c8dce6d95f
adriantech-beep <Adhrianne29@gmail.com>
2025-01-03 18:02:59 +0800

    fixed file path of logo brand

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,7 +2,7 @@ function Nav() {
   return (
     <nav className="nav">
       <img
-        src="/src/assets/uplift-nav.webp"
+        src="./src/assets/uplift-nav.webp"
         alt="A logo brand of Uplift Code Camp"
       />
     </nav>
